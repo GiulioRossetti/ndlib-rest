1047f4c96779dc3e45afd23632ffe0b313146674
Giulio Rossetti <giulio.rossetti@gmail.com>
2016-06-25 14:39:23 +0200

    Generalized Client Example

--- client/example.py
+++ client/example.py
@@ -4,15 +4,15 @@ __author__ = 'rossetti'
 __license__ = "GPL"
 __email__ = "giulio.rossetti@gmail.com"
 
-e = NDlibClient("http://kddsna.isti.cnr.it:5000")
+e = NDlibClient("http://127.0.0.1:5000")
 e.create_experiment()
 
-#e.add_barabasi_albert_graph(4000, 10)
-#e.add_threshold_model(0.01)
-#for it in xrange(0, 20):
-#    ex = e.get_iteration()
-#    for m in ex:
-#        print it, m, sum(ex[m]['status'].values())
+# e.add_barabasi_albert_graph(4000, 10)
+# e.add_threshold_model(0.01)
+# for it in xrange(0, 20):
+#     ex = e.get_iteration()
+#     for m in ex:
+#         print it, m, sum(ex[m]['status'].values())
 
 e.load_exploratory("Lastfm_rock")
 e.add_threshold_model(0.01)
