af21a6eb25dbb6b281f7c13e9c746de5d6399505
Giulio Rossetti <giulio.rossetti@gmail.com>
2017-01-23 17:02:35 +0100

    + Exploratory bug fix

--- client/example.py
+++ client/example.py
@@ -5,30 +5,51 @@ __license__ = "GPL"
 __email__ = "giulio.rossetti@gmail.com"
 
 e = NDlibClient("http://127.0.0.1:5000")
+
+print "Core"
 e.create_experiment()
-x = e.load_exploratory("Lastfm_rock")
-print x
+x = e.load_exploratory("ToyCore_Com")
 
 # e.add_barabasi_albert_graph(4000, 10)
 # e.add_planted_lpartition_graph(30, 35, 0.7, 0.2)
-e.add_kertesz_model(0.1, threshold=0.1, blocked=0.1, adopter_rate=0.1)
+# e.add_kertesz_model(0.1, threshold=0.1, blocked=0.1, adopter_rate=0.1)
+e.add_threshold_model(0.1, threshold=0.16)
 
-print "here"
+
+# print "here"
 # g = e.get_graph()
 # conf = {'model': {'blocked': g.nodes()[5:30]}}
 # p = e.set_advanced_configuration(conf)
-for it in xrange(0, 10):
+tot = 0
+for it in xrange(0, 30):
     ex = e.get_iteration()
     for m in ex:
-        blocked = [k for v, k in ex[m]['status'].iteritems() if k == -1]
-        print len(blocked)
-        print it, ex[m]['status']
+        # blocked = [v for v, k in ex[m]['status'].iteritems() if k == 1]
+        # print blocked
+        # print len(blocked)
+        tot += sum(ex[m]['status'].values())
+        print tot
+
+e.destroy_experiment()
+
+print "Periphery"
+e.create_experiment()
+x = e.load_exploratory("ToyPeri_Com")
+e.add_threshold_model(0.1, threshold=0.16)
+tot = 0
+for it in xrange(0, 40):
+    ex = e.get_iteration()
+    for m in ex:
+        # blocked = [v for v, k in ex[m]['status'].iteritems() if k == 1]
+        # print blocked
+        # print len(blocked)
+        tot += sum(ex[m]['status'].values())
+        print tot
 
 e.destroy_experiment()
 
 exit()
 
-print "reset"
 e.reset_experiment()
 for it in xrange(0, 10):
     ex = e.get_iteration()
