48a125f32ad544cb7aa0f6022ebe64cf0ed27027
Giulio Rossetti <giulio.rossetti@gmail.com>
2018-09-25 17:33:09 +0200

    Update gunicorn.sh

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
-exec gunicorn -w 4 -b 0.0.0.0:5000 ndrest:app &
-cd NDLib_viz
-exec npm run dev
+
+pip install -r requirements.txt 
+exec gunicorn -w 4 -b 0.0.0.0:5000 ndrest:app > /dev/null &
+
+if [ -d NDLib_viz ]; then 
+  rm -rf NDLib_viz 
+fi
+
+git clone https://github.com/GiulioRossetti/NDLib_viz.git > /dev/null 
+cd NDLib_viz 
+npm install 
+npm run dev &
